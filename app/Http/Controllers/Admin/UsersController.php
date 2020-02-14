fa78f3ed92593d0fd497bf240d0e9462aa712a84
dianynf <dianbugas@gmail.com>
2020-02-14 11:24:18 +0700

    role admin,auther,and-user

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\Role;
+use Gate;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -66,6 +67,10 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        if (Gate::denies('edit-users')) {
+            return redirect(route('admin.users.index'));
+        }
+
         $roles = Role::all();
 
         return view('admin.users.edit')->with([
@@ -83,7 +88,8 @@ class UsersController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->roles()->sync($request->roles);
+        return redirect()->route('admin.users.index');
     }
 
     /**
@@ -94,6 +100,13 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        if (Gate::denies('delete-users')) {
+            return redirect(route('admin.users.index'));
+        }
+
+        $user->roles()->detach();
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
     }
 }
